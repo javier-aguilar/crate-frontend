0db1736b20eb5fb145eab70b5d9d6358f8998671
Javier Aguilar <jvr.agx@gmail.com>
2020-07-03 14:26:48 -0600

    Update app_url_api

--- src/setup/config/env.js
+++ src/setup/config/env.js
@@ -6,7 +6,7 @@ dotenv.config()
 
 // URL
 export const APP_URL = process.env.APP_URL
-export const APP_URL_API = process.env.APP_URL_API
+export const APP_URL_API = "https://powerful-citadel-80950.herokuapp.com/"
 
 // Environment
 export const NODE_ENV = process.env.NODE_ENV
